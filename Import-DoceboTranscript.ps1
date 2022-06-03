61cf4850be372c45a63e0f3104e417fbb63b236d
Ben Climer <84200635+shiftnerd@users.noreply.github.com>
2022-06-03 03:03:03 -0500

    Update Import-DoceboTranscript.ps1

--- Import-DoceboTranscript.ps1
+++ Import-DoceboTranscript.ps1
@@ -8,10 +8,11 @@ function Import-DoceboTranscript {
         [Parameter(Mandatory,HelpMessage='Enter the bearer token!')] [string[]] $bearer_token,
         [Parameter(Mandatory,HelpMessage='Enter the course code!')] [string[]] $CourseCode,
         [Parameter(Mandatory,HelpMessage='Enter the Import CSV Path!')] [string[]] $ImportPath,
-        $Url = "https://UPDATEURLHERE.docebosaas.com/learn/v1/enrollment/batch"
+        $Url = "https://UPDATEURLHERE.docebosaas.com/learn/v1/enrollment/batch",
+        $CSVHeaders = "USERNAME","LASTNAME","FIRSTNAME","DATEENROLLED","DATECOMPLETED","STATUS","SCORE"
     )
     $APIHeaders = @{Authorization = "Bearer $bearer_token"}
-    $CSVHeaders = "USERNAME","LASTNAME","FIRSTNAME","DATEENROLLED","DATECOMPLETED","STATUS","SCORE"
+
 
     Import-CSV -Path "$ImportPath" -Header $CSVHeaders | ForEach-Object {
 
